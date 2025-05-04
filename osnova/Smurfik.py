af2da347ba77b181fdfba72b584f853c6976c79f
1RAFTIK1 <talapov.d.v@edu.mirea.com>
2025-05-05 01:41:07 +0300

    quick_save

--- osnova/Smurfik.py
+++ osnova/Smurfik.py
@@ -31,14 +31,35 @@ def start(message):
         reply_markup=markup
     )
     
-@bot.message_handler()
-def add_reminder_step1()
-def add_reminder_step2()
-def add_reminder_step3()
-def add_reminder_step4()
+@bot.message_handler(func=lambda message: message.text == '➕ Добавить напоминание')
+def add_reminder_step1(message):
+    msg = bot.send_message(message.chat.id, "Напиши текст напоминания:")
+    bot.register_next_step_handler(msg, add_reminder_step2)
+    
+    
+def add_reminder_step2(message):
+    text = message.text
+    if len(text) > 500:
+        bot.send_message(message.chat.id, "Текст слишком длинный (макс. 500 символов)")
+        return
+    
+    markup = types.ReplyKeyboardMarkup(resize_keyboard=True, one_time_keyboard=True)
+    for i in range(6):
+        markup.add(types.KeyboardButton(str(i)))
+    
+    msg = bot.send_message(
+         message.chat.id,
+        "Выбери приоритет (0-5), где 0 - низкий, 5 - очень высокий:",
+        reply_markup=markup
+    )
+    bot.register_next_step_handler(msg, add_reminder_step3)
+    
+def add_reminder_step3(message):
+def add_reminder_step4(message):
 
-@bot.message_handler()
-def show_reminders()
+
+@bot.message_handler(func=lambda message: message.text == '📋 Мои напоминания')
+def show_reminders(message):
 
 if __name__ == '__main__':
     print("Bot is cooking!")
