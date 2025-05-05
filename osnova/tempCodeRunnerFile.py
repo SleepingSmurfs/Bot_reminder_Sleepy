223bc85e66026baed6f128188eaeeb870e71b5da
1RAFTIK1 <134624191+1RAFTIK1@users.noreply.github.com>
2025-05-05 03:24:10 +0300

    Delete osnova/tempCodeRunnerFile.py

--- osnova/tempCodeRunnerFile.py
+++
@@ -1,8 +0,0 @@
-def scheduler():
-#     schedule.every().day.at("08:00").do(send_daily_reminders)
-    
-#     while True:
-#         schedule.run_pending()
-#         time.sleep(1)
-
-# threading.Thread(target=scheduler, daemon=True).start()
