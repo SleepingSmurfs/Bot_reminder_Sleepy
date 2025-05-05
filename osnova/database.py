1572d768cbc40c9deba03a03c303a7d69d6e4e69
1RAFTIK1 <talapov.d.v@edu.mirea.com>
2025-05-05 03:20:22 +0300

    logika bd i funcionale

--- osnova/database.py
+++ osnova/database.py
@@ -45,20 +45,75 @@ class Database:
         
     def create_tables(self):
         with self.conn.cursor() as cur:
+            cur.execute("""
+                CREATE TABLE IF NOT EXISTS users (
+                    user_id BIGINT PRIMARY KEY,
+                    username TEXT,
+                    first_name TEXT,
+                    last_name TEXT,
+                    registered_at TIMESTAMP DEFAULT NOW()
+                )
+            """)
+            
+            cur.execute("""
+                CREATE TABLE IF NOT EXISTS reminders (
+                    id SERIAL PRIMARY KEY,
+                    user_id BIGINT REFERENCES users(user_id),
+                    text TEXT NOT NULL,
+                    priority INTEGER NOT NULL CHECK (priority BETWEEN 0 AND 5),
+                    created_at TIMESTAMP DEFAULT NOW(),
+                    expires_at TIMESTAMP NOT NULL,
+                    is_completed BOOLEAN DEFAULT FALSE
+                )
+            """)
+            self.conn.commit()
+            logger.info("Таблицы созданы/проверены")
     
     
     
-    def execute_safe()
+    def execute_safe(self, query, params=None):
+        df
     
-    def fetch_safe()
+    def fetch_safe(self, query, params=None):
+        try:
+            with self.conn.cursor() as cur:
+                cur.execute(query, params or ())
+                return cur.fetchall()
+        except psycopg2.Error as e:
+            logger.error(f"Ошибка базы данных: {e}")
+            return None
+        
+        
+    def add_user(self,  user_id, username, first_name, last_name):
+        df
+        
+        
+    def add_reminder(self, user_id, text, priority, days):
+        expires_at = datetime.now() + timedelta(days=days)
+        result = self.fetch_safe("""
+            INSERT INTO reminders (user_id, text, priority, expires_at)
+            VALUES (%s, %s, %s, %s)
+            RETURNING id
+        """, (user_id, text, priority, expires_at))
+        return result[0][0] if result else None
+        
+        
+    def get_today_reminders(self, user_id):
+        today = datetime.now().date()
+        tomorrow = today + timedelta(days=1)
+        return self.fetch_safe("""
+            SELECT id, text, priority
+            FROM reminders
+            WHERE user_id = %s 
+            AND expires_at BETWEEN %s AND %s
+            AND is_completed = FALSE
+            ORDER BY priority DESC
+        """, (user_id, today, tomorrow))
     
-    def add_user()
     
-    def add_reminder()
-    
-    def get_today_reminders()
-    
-    def get_all_users()
+    def get_all_users(self):
+        result = self.fetch_safe("SELECT user_id FROM users")
+        return [row[0] for row in result] if result else []
     
     # def delete_reminder()
     
